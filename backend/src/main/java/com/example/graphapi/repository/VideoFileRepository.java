56cdefa40104d707c81ea64b85ccb9145c1a381f
Vineet Kumar Mishra <vineet.mishra@irissoftware.com>
2025-08-25 23:56:38 +0530

    dashboard files

--- backend/src/main/java/com/example/graphapi/repository/VideoFileRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.graphapi.repository;
-
-import com.example.graphapi.entity.VideoFiles;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface VideoFileRepository extends JpaRepository<VideoFiles, Long> {}
